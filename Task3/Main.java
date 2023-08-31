79588a15b13be92f2adf60a32a00bf9ea26c0321
Minal Meshram <96578677+minalmeshram28@users.noreply.github.com>
2023-08-31 21:23:01 +0530

    Update Main.java

--- Task3/Main.java
+++ Task3/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount userAccount = new BankAccount(1000.0); // Initial balance
+        BankAccount userAccount = new BankAccount(1000.0); 
         ATM atm = new ATM(userAccount);
         atm.run();
     }
